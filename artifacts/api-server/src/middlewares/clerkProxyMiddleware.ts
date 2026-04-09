63cdc5644730200541259d80a8a1495dab5bdd14
ivyxlo <ivylo2315@gmail.com>
2026-04-08 21:44:59 -0700

    auth fix 2

--- artifacts/api-server/src/middlewares/clerkProxyMiddleware.ts
+++ artifacts/api-server/src/middlewares/clerkProxyMiddleware.ts
@@ -19,7 +19,7 @@
 import { createProxyMiddleware } from "http-proxy-middleware";
 import type { RequestHandler } from "express";
 
-const CLERK_FAPI = "https://clerk.triad.ezamu.com";
+const CLERK_FAPI = "https://frontend-api.clerk.dev";
 export const CLERK_PROXY_PATH = "/api/__clerk";
 
 export function clerkProxyMiddleware(): RequestHandler {
