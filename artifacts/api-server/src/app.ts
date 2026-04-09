dde9f5a0268bf22fbe68ddb3ef4fd0a12c8f866b
ivyxlo <ivylo2315@gmail.com>
2026-04-08 22:08:49 -0700

    auth fix 3

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -34,7 +34,7 @@ app.use(
   }),
 );
 
-app.use(CLERK_PROXY_PATH, clerkProxyMiddleware());
+//app.use(CLERK_PROXY_PATH, clerkProxyMiddleware());
 
 app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
